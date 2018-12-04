fdfe95dd3a17b55d5c1d86eabd11a393d1a5cb6b
acedeywin <stephenokpala@gmail.com>
2018-12-04 16:17:55 +0100

    new commit

---
+++ scripts/myScript.js
@@ -0,0 +1,74 @@
+let myName = document.getElementById('my-name');
+myName.textContent = 'Okpalaononuju, Stephen Ifeamarame';
+
+let jobTitle = document.getElementById('job-title');
+jobTitle.textContent = 'Research Assistant/Tech Savvy';
+
+let myLocation = document.getElementById('my-location');
+myLocation.textContent = 'Kano, Nigieria';
+
+let myEmail = document.getElementById('my-email');
+myEmail.textContent = 'stephenokpala@gmail.com';
+
+let myNumber = document.getElementById('my-number');
+myNumber.textContent = '+234 (0) 806 546 7341';
+
+let myEducation = document.getElementById('my-education');
+myEducation.textContent = 'Education';
+
+let	firstSchool, secondSchool, thirdSchool;
+if(firstSchool = document.getElementById('first-school')){
+    firstSchool.textContent = 'Federal University, Dustin-Ma';
+}
+if(secondSchool = document.getElementById('second-school')){
+    secondSchool.textContent = 'Nnamdi Azikiwe University, Awka';
+    }
+if(thirdSchool = document.getElementById('third-school')){
+    thirdSchool.textContent = 'Abbas Maje Nasarrawa Secondary School';
+}    
+        
+let schoolDate1, schoolDate2, schoolDate3;
+if(schoolDate1 = document.getElementById('school-date1')){
+    schoolDate1.textContent = 'Feb 2018 - May 2019';
+}
+if(schoolDate2 = document.getElementById('school-date2')){
+    schoolDate2.textContent = 'Oct 2010 - Sep 2014';
+}
+if(schoolDate3 = document.getElementById('school-date3')){
+    schoolDate3.textContent = 'Sep 2001 - Jul 2007';
+}
+
+let mastersDegree = document.getElementById('masters');
+mastersDegree.textContent = 'Masters of Development Studies';
+
+let bachDegree = document.getElementById('bachelor');
+bachDegree.textContent = 'Bachelor of Science in Psychology';
+
+let olevelCert = document.getElementById('olevel');
+olevelCert.textContent = 'O’level (WASSC), SLC&T';
+
+let skills = ['Skills','Microsoft Office Suit','Content Management System','ODK Tools | KOBO Tools','HTML5 | CSS3 | JavaScript','Google Cloud Platform','LAN Networking','PHP | Python | MATLAB | Octave'];
+
+let skill = document.getElementById('skill');
+skill.textContent = skills[0];
+
+let skill1 = document.getElementById('skill1');
+skill1.textContent = skills[1];
+
+let skill2 = document.getElementById('skill2');
+skill2.textContent = skills[2];
+
+let skill3 = document.getElementById('skill3');
+skill3.textContent = skills[3];
+
+let skill4 = document.getElementById('skill4');
+skill4.textContent = skills[4];
+
+let skill5 = document.getElementById('skill5');
+skill5.textContent = skills[5];
+
+let skill6 = document.getElementById('skill6');
+skill6.textContent = skills[6];
+
+let skill7 = document.getElementById('skill7');
+skill7.textContent = skills[7];
