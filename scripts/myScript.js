619f2408a3ff56884e554a7c2c6f22245dd8f2e0
Stephen Okpalaononuju <stephenokpala@gmail.com>
2020-09-21 16:42:54 +0100

    New updates

--- scripts/myScript.js
+++ scripts/myScript.js
@@ -1,136 +1,165 @@
-let myName = document.getElementById('my-name');
-myName.textContent = 'Okpalaononuju, Stephen Ifeamarame';
+let myName = document.getElementById("my-name");
+myName.textContent = "Okpalaononuju, Stephen Ifeamarame";
 
-let jobTitle = document.getElementById('job-title');
-jobTitle.textContent = 'Designer | Developer | Behavioural Research';
+let jobTitle = document.getElementById("job-title");
+jobTitle.textContent = "Designer | Developer | Behavioural Research";
 
-let myLocation = document.getElementById('my-location');
-myLocation.textContent = 'Kano, Nigieria';
+let myLocation = document.getElementById("my-location");
+myLocation.textContent = "Kano, Nigieria";
 
-let myEmail = document.getElementById('my-email');
-myEmail.textContent = 'stephenokpala@gmail.com';
+let myEmail = document.getElementById("my-email");
+myEmail.textContent = "stephenokpala@gmail.com";
 
-let myNumber = document.getElementById('my-number');
-myNumber.textContent = '+234 (0) 806 546 7341';
+let myNumber = document.getElementById("my-number");
+myNumber.textContent = "+234 (0) 806 546 7341";
 
-let myEducation = document.getElementById('my-education');
-myEducation.textContent = 'Education';
+let myEducation = document.getElementById("my-education");
+myEducation.textContent = "Education";
 
-let	firstSchool, secondSchool, thirdSchool;
-if(firstSchool = document.getElementById('first-school')){
-    firstSchool.textContent = 'Federal University, Dustin-Ma';
+let fullStack, firstSchool, secondSchool, thirdSchool;
+if ((fullStack = document.getElementById("fullstack-school"))) {
+  fullStack.textContent =
+    "The Hong Kong University of Science and Technology via Cousera.com";
 }
-if(secondSchool = document.getElementById('second-school')){
-    secondSchool.textContent = 'Nnamdi Azikiwe University, Awka';
-    }
-if(thirdSchool = document.getElementById('third-school')){
-    thirdSchool.textContent = 'Abbas Maje Nasarrawa Secondary School';
-}    
-        
-let schoolDate1, schoolDate2, schoolDate3;
-if(schoolDate1 = document.getElementById('school-date1')){
-    schoolDate1.textContent = 'Feb 2018 - Sep 2019';
+if ((firstSchool = document.getElementById("first-school"))) {
+  firstSchool.textContent = "Federal University, Dustin-Ma";
 }
-if(schoolDate2 = document.getElementById('school-date2')){
-    schoolDate2.textContent = 'Oct 2010 - Sep 2014';
+if ((secondSchool = document.getElementById("second-school"))) {
+  secondSchool.textContent = "Nnamdi Azikiwe University, Awka";
 }
-if(schoolDate3 = document.getElementById('school-date3')){
-    schoolDate3.textContent = 'Sep 2001 - Jul 2007';
+if ((thirdSchool = document.getElementById("third-school"))) {
+  thirdSchool.textContent = "Abbas Maje Nasarrawa Secondary School";
 }
 
-let mastersDegree = document.getElementById('masters');
-mastersDegree.textContent = 'Masters of Development Studies';
+let fullStackDate, schoolDate1, schoolDate2, schoolDate3;
+if ((fullStackDate = document.getElementById("fullstack-date"))) {
+  fullStackDate.textContent = "Sep 2020 - ";
+}
+if ((schoolDate1 = document.getElementById("school-date1"))) {
+  schoolDate1.textContent = "Feb 2018 - Sep 2019";
+}
+if ((schoolDate2 = document.getElementById("school-date2"))) {
+  schoolDate2.textContent = "Oct 2010 - Sep 2014";
+}
+if ((schoolDate3 = document.getElementById("school-date3"))) {
+  schoolDate3.textContent = "Sep 2001 - Jul 2007";
+}
 
-let bachDegree = document.getElementById('bachelor');
-bachDegree.textContent = 'Bachelor of Science in Psychology';
+let special = document.getElementById("special");
+special.textContent = "Full-Stack Web Development with React Specialization";
 
-let olevelCert = document.getElementById('olevel');
-olevelCert.textContent = 'O’level (WASSC), SLC&T';
+let mastersDegree = document.getElementById("masters");
+mastersDegree.textContent = "Masters of Development Studies";
 
-let skills = ['Technology Skills','HTML5 | CSS3','JavaScript | JQuery','Angular | React | SCSS','NodeJS','Python | Php','Google Cloud Platform | PostgreSQL | MongoDB','Git | Heroku','Webpack | Swagger | TravisCI','Mocha | Jasmine','Unit Test | TDD','CorelDraw | Adobe XD | LogoDesign Shop','Wordpress | Wix','Microsoft Office Suit','ODK Tools | KOBO Tools'];
+let bachDegree = document.getElementById("bachelor");
+bachDegree.textContent = "Bachelor of Science in Psychology";
 
-let skillList = ['non','Frontend','Backend','Database','DevOps','Test Technique & Framework','Design','Content Management System','Others'];
+let olevelCert = document.getElementById("olevel");
+olevelCert.textContent = "O’level (WASSC), SLC&T";
 
-let skill = document.getElementById('skill');
+let skills = [
+  "Technology Skills",
+  "HTML5 | CSS3",
+  "JavaScript | JQuery",
+  "Angular | React | SCSS",
+  "NodeJS",
+  "Python | Php",
+  "Google Cloud Platform | PostgreSQL | MongoDB",
+  "Git | Heroku",
+  "Webpack | Swagger | TravisCI",
+  "Mocha | Jasmine",
+  "Unit Test | TDD",
+  "CorelDraw | Adobe XD | LogoDesign Shop",
+  "Wordpress | Wix",
+  "Microsoft Office Suit",
+  "ODK Tools | KOBO Tools",
+];
+
+let skillList = [
+  "non",
+  "Frontend",
+  "Backend",
+  "Database",
+  "DevOps",
+  "Test Technique & Framework",
+  "Design",
+  "Content Management System",
+  "Others",
+];
+
+let skill = document.getElementById("skill");
 skill.textContent = skills[0];
 
 //list 1
-let skillList1 = document.getElementById('skill-list1');
+let skillList1 = document.getElementById("skill-list1");
 skillList1.textContent = skillList[1];
 
-let skill1 = document.getElementById('skill1');
+let skill1 = document.getElementById("skill1");
 skill1.textContent = skills[1];
 
-let skill2 = document.getElementById('skill2');
+let skill2 = document.getElementById("skill2");
 skill2.textContent = skills[2];
 
-let skill3 = document.getElementById('skill3');
+let skill3 = document.getElementById("skill3");
 skill3.textContent = skills[3];
 
 //list 2
-let skillList2 = document.getElementById('skill-list2');
+let skillList2 = document.getElementById("skill-list2");
 skillList2.textContent = skillList[2];
 
-let skill4 = document.getElementById('skill4');
+let skill4 = document.getElementById("skill4");
 skill4.textContent = skills[4];
 
-let skill5 = document.getElementById('skill5');
+let skill5 = document.getElementById("skill5");
 skill5.textContent = skills[5];
 
 //list 3
-let skillList3 = document.getElementById('skill-list3');
+let skillList3 = document.getElementById("skill-list3");
 skillList3.textContent = skillList[3];
 
-let skill6 = document.getElementById('skill6');
+let skill6 = document.getElementById("skill6");
 skill6.textContent = skills[6];
 
 //list 4
-let skillList4 = document.getElementById('skill-list4');
+let skillList4 = document.getElementById("skill-list4");
 skillList4.textContent = skillList[4];
 
-let skill7 = document.getElementById('skill7');
+let skill7 = document.getElementById("skill7");
 skill7.textContent = skills[7];
 
-let skill8 = document.getElementById('skill8');
+let skill8 = document.getElementById("skill8");
 skill8.textContent = skills[8];
 
 //list 5
-let skillList5 = document.getElementById('skill-list5');
+let skillList5 = document.getElementById("skill-list5");
 skillList5.textContent = skillList[5];
 
-let skill9 = document.getElementById('skill9');
+let skill9 = document.getElementById("skill9");
 skill9.textContent = skills[9];
 
-let skill10 = document.getElementById('skill10');
+let skill10 = document.getElementById("skill10");
 skill10.textContent = skills[10];
 
 //list 6
-let skillList6 = document.getElementById('skill-list6');
+let skillList6 = document.getElementById("skill-list6");
 skillList6.textContent = skillList[6];
 
-let skill11 = document.getElementById('skill11');
+let skill11 = document.getElementById("skill11");
 skill11.textContent = skills[11];
 
 //list 7
-let skillList7 = document.getElementById('skill-list7');
+let skillList7 = document.getElementById("skill-list7");
 skillList7.textContent = skillList[7];
 
-let skill12 = document.getElementById('skill12');
+let skill12 = document.getElementById("skill12");
 skill12.textContent = skills[12];
 
 //list 8
-let skillList8 = document.getElementById('skill-list8');
+let skillList8 = document.getElementById("skill-list8");
 skillList8.textContent = skillList[8];
 
-let skill13 = document.getElementById('skill13');
+let skill13 = document.getElementById("skill13");
 skill13.textContent = skills[13];
 
-let skill14 = document.getElementById('skill14');
+let skill14 = document.getElementById("skill14");
 skill14.textContent = skills[14];
-
-
-
-
-
-
-
